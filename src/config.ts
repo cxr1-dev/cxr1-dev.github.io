5df9899229265a560937cc281d8c49975d2104f9
cxr1-dev <106861368@qq.com>
2025-11-14 17:47:57 +0800

    change bio

--- src/config.ts
+++ src/config.ts
@@ -66,7 +66,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/cxr1.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Cxr1",
-	bio: "Keep U ❤.",
+	bio: "Keep U ❤ & Stay Alive.",
 	links: [
 		{
 			name: "Bilibili",
